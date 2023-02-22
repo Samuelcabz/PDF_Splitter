27bdd7880cb883bfa01ca55e9b3d2e19753fb9c2
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-02-22 17:49:57 +0800

    Update pdfsplitter.py

--- pdfsplitter.py
+++ pdfsplitter.py
@@ -3,8 +3,8 @@ from PyPDF2 import PdfFileWriter,PdfFileReader
 def cropper(start,end,file):
     inputPdf = PdfFileReader(open(file,"rb"))
     outPdf = PdfFileWriter()
-    fnamestart = start +1
-    fnameend = end +1
+    fnamestart = "{}{}".format(start+1)
+    fnameend = "{}{}".format(end,1)
     ostream=open(file.split(".")[0]+"_Page_"+str(fnamestart)+"_"+str(fnameend)+".pdf","wb")
 
     while start <= end:
