7994948d0123f180f8b1e98f5bb4dc26ab729db0
Samuel Alingasa Cabagay <samuelcabagay707@gmail.com>
2023-09-13 16:39:21 +0800

    s

--- flask_app.py
+++ flask_app.py
@@ -6,35 +6,24 @@ st1 = None
 en1 = None
 file = None
 
+
 @app.route("/")
-def upload():
-    return render_template("file_upload.html")
+def login():
+    return render_template("login.html")
 
+@app.route("/welcomepage")
+def welcomepage():
+    return render_template("welcomepage.html")
 
-@app.route("/success",methods=["POST"])
-def success():
-    st=int(request.form['start'])
-    en=int(request.form['end'])
-    st1=st-1
-    en1=en-1
-    print(st1)
-    print(en1)
-    f=request.files['file']
-    file=f.filename
-    f.save(file)
-    return render_template("success.html",start=st,end=en,name=file)
+@app.route("/checkform", methods=["POST"])
+def checkform():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route("/convert/<st1>/<en1>/<file>")
-def cropper(st1,en1,file):
-    pdfsplitter.cropper(st1,en1,file)
-    return render_template("download.html",st1=st1,en1 = en1,file=file)
+        if username == 'via' and password == 'yellow':
+            return redirect("/welcomepage")
 
+    return redirect("/") 
 
-
-@app.route("/download/<st1>/<en1>/<file>")
-def download(st1,en1,file):
-    fnamestart = int(st1) +1
-    fnameend = int(en1) +1
-    filename=file.split(".")[0]+"-Page-"+str(fnamestart)+"-"+str(fnameend)+".pdf"
-    return send_file("/home/samieeee/PDF_Splitter/"+filename,as_attachment=True)
 
